0af6b260c154384224f96fd5428572cfb1e39b1c
Aswin0709 <aswinku2000@gmail.com>
2021-06-29 20:47:07 +0530

    first commit

--- correlation.py
+++ correlation.py
@@ -1,1 +1,11 @@
 # Add the functions in this file
+import json
+
+def load_journal():
+	ret=[]
+	f = open('journal.json',)
+	data = json.load(f)
+	for i in data:
+		ret.append(i)
+	f.close()
+	print (ret) 
